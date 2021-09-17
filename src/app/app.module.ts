46d859428d5a01a26f9e81a336655da22683c4af
wsztejnberg <williamsztejnberg@gmail.com>
2021-09-17 00:26:12 -0400

    Updates to UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GithubService } from "./github.service";
+import { PieChartComponent } from "./pie-chart/pie-chart.component";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import {ChartsModule} from "@progress/kendo-angular-charts";
+
+import "hammerjs"
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ChartsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    GithubService,
+    HttpClientModule,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/lang-stats.ts
@@ -0,0 +1,8 @@
+export class LangStats {
+  category: String
+  value: number
+  constructor(category: String, value: number) {
+    this.category = category
+    this.value = value
+  }
+}
